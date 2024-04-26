08807160f976a2b403e02d5f261a75cc583f3406
nikithayohannan <125458671+nikithayohannan@users.noreply.github.com>
2024-04-26 23:03:42 +0530

    Add files via upload

---
+++ fakejob/app/serializer.py
@@ -0,0 +1,7 @@
+# from rest_framework import serializers
+# from .models import *
+
+# class ReactSerializer(serializers.ModelSerializer):  # Corrected import
+#     class Meta:
+#         model = React
+#         fields = ['email', 'password']
